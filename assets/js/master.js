c653766cb5a56c613c1f1b1a2cdfd1173ccc4e3e
Thiago Ricieri <thiago.ricieri@gmail.com>
2018-05-11 13:39:14 -0300

    Add MailChimp links

--- assets/js/master.js
+++ assets/js/master.js
@@ -9,20 +9,3 @@ var clickFun = function(modal){
     al.alert();
   }
 };
-var sendingState = function(btn) {
-  $(btn).attr('disabled', true);
-  $(btn).html('Enviando...');
-};
-var sentState = function(btn) {
-  $(btn).attr('disabled', false);
-  $(btn).html('Enviar');
-};
-
-var applyActions = function() {
-  for(var i = 0; i<modals.length; i++) {
-    var modal = modals[i];
-    $('#'+modal+'Modal .btn').click(clickFun(modal));
-  }
-};
-
-applyActions();
